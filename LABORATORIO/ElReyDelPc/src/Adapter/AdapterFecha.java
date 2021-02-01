aa68339e9ceca745264fc0be6cc5bddf5a8ebff7
MarioAdanH <mario.adan@edu.uah.es>
2021-02-01 13:51:48 +0100

    asdf

--- LABORATORIO/ElReyDelPc/src/Adapter/AdapterFecha.java
+++ LABORATORIO/ElReyDelPc/src/Adapter/AdapterFecha.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Adapter;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.StringTokenizer;
 
-/**
- *
- * @author Mario
- */
 public class AdapterFecha implements Fecha {
     
     private Date fecha;
